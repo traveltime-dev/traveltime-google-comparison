b2ccd08bf3487b229cfd40c8e4ff1cdb328612c4
arnasbr <arnas.bradauskas@traveltime.com>
2024-09-26 16:12:12 +0300

    more tests

--- test/test_config.py
+++ test/test_config.py
@@ -1,3 +1,5 @@
+import pytest
+
 from traveltime_google_comparison.config import (
     Provider,
     Providers,
@@ -46,4 +48,59 @@ def test_json_config_parse():
                 name="google", max_rpm=60, credentials=Credentials("<your-api-key>")
             )
         ],
+    )
+
+
+def test_json_config_parse_all_disabled_providers():
+    json = """
+        {
+          "traveltime": {
+            "app-id": "<your-app-id>",
+            "api-key": "<your-api-key>",
+            "max-rpm": "60"
+          },
+          "api-providers": [
+            {
+              "name": "google",
+              "enabled": false,
+              "api-key": "<your-api-key>",
+              "max-rpm": "60"
+            },
+            {
+              "name": "tomtom",
+              "enabled": false,
+              "api-key": "<your-api-key>",
+              "max-rpm": "30"
+            }
+          ]
+        }
+    """
+
+    with pytest.raises(ValueError) as excinfo:
+        _ = parse_json_to_providers(json)
+
+    assert (
+        str(excinfo.value)
+        == "There should be at least one enabled API provider that's not TravelTime."
+    )
+
+
+def test_json_config_parse_empty_providers():
+    json = """
+        {
+          "traveltime": {
+            "app-id": "<your-app-id>",
+            "api-key": "<your-api-key>",
+            "max-rpm": "60"
+          },
+          "api-providers": []
+        }
+    """
+
+    with pytest.raises(ValueError) as excinfo:
+        _ = parse_json_to_providers(json)
+
+    assert (
+        str(excinfo.value)
+        == "There should be at least one enabled API provider that's not TravelTime."
     )
